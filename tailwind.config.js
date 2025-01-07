21e9b0dcec43d55ff969ffb2c9eeed349411be6c
Hosuqa <kuba.grabowski129@gmail.com>
2025-01-07 19:53:39 +0100

    [INIT]

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Pages/**/*.{razor,html,cshtml}",
+    "./Shared/**/*.{razor,html,cshtml}",
+    "./wwwroot/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
