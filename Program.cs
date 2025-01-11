e96b283a99bcf257c868525ac671da0a3d220128
Michał Świercz <loolok1232@gmail.com>
2025-01-11 17:58:13 +0100

    [ADD] - cart working

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<PizzaService>();
+builder.Services.AddScoped<CartService>();
+
 
 var app = builder.Build();
 
---
+++ Services/CartService.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+
+namespace MarioPizzeria.Services
+{
+    public class CartService
+    {
+        public List<CartItem> Items { get; private set; } = new List<CartItem>();
+
+        public void AddToCart(int pizzaId, string name, string description, int quantity, float price)
+        {
+            var existingItem = Items.Find(item => item.PizzaId == pizzaId);
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+                existingItem.TotalPrice += quantity * price;
+            }
+            else
+            {
+                Items.Add(new CartItem
+                {
+                    PizzaId = pizzaId,
+                    Name = name,
+                    Description = description,
+                    Quantity = quantity,
+                    TotalPrice = quantity * price,
+                });
+            }
+        }
+
+        public void ClearCart()
+        {
+            Items.Clear();
+        }
+    }
+
+    public class CartItem
+    {
+        public int PizzaId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public float TotalPrice { get; set; }
+
+    }
+}
